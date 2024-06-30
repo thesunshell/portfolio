62e2f6f53857ca01814b3f17456b16b6fca5ef0b
acx-sunny <sunny.kumar@acxhange.com>
2024-06-30 15:12:24 +0530

    First Deploy

---
+++ docs/browser/chunk-KCXFTYNE.js
@@ -0,0 +1,1 @@
+import{Ha as i,Ia as a,La as r,Oa as s,W as o}from"./chunk-LGJ7LFG4.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-home"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(i(0,"p"),r(1,"home works!"),a())}});let t=e;return t})();export{d as HomeComponent};
