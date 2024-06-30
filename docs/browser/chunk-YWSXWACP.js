4a14ceba255fe63661ee6c8c7388b06e60531f0e
acx-sunny <sunny.kumar@acxhange.com>
2024-06-30 16:21:51 +0530

    First Deploy

---
+++ docs/browser/chunk-YWSXWACP.js
@@ -0,0 +1,1 @@
+import{Ha as a,Ia as i,La as r,Oa as s,W as o}from"./chunk-LGJ7LFG4.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-about-me"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,p){n&1&&(a(0,"p"),r(1,"about-me works!"),i())}});let t=e;return t})();export{d as AboutMeComponent};
